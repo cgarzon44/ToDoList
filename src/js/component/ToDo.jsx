3e8dd78739602523700e1c4eac741a5ad89ef557
cgarzon44 <cgarzon44@gmail.com>
2021-11-14 01:49:59 +0000

    got it

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -3,21 +3,22 @@ import React, { useState, useRef } from "react";
 //create your first component
 
 function ToDo({ todos, completeTodo, removeTodo, props }) {
-	const [isShownHoverContent, setIsShownHoverContent] = useState(false);
+	const [isShownHoverContent, setIsShownHoverContent] = useState(-1);
 
 	return todos.map(todo => (
 		<div
 			className="d-flex"
 			key={todo.id}
-			onMouseEnter={() => setIsShownHoverContent(true)}
-			onMouseLeave={() => setIsShownHoverContent(false)}>
-			<div className="p-2 w-100" onClick={() => completeTodo(todo.id)}>
+			onMouseEnter={() => setIsShownHoverContent(todo.id)}
+			onMouseLeave={() => setIsShownHoverContent(-1)}>
+			<div className="p-2 w-100">
 				<ul
 					className={
 						todo.isComplete
 							? "list-group list-group-flush complete"
 							: "list-group list-group-flush"
-					}>
+					}
+					onClick={() => completeTodo(todo.id)}>
 					<li className="list-group-item">{todo.text}</li>
 				</ul>
 			</div>
@@ -26,7 +27,7 @@ function ToDo({ todos, completeTodo, removeTodo, props }) {
 				<i
 					onClick={() => removeTodo(todo.id)}
 					className={
-						isShownHoverContent
+						isShownHoverContent === todo.id
 							? "fas fa-times p-2 flex-shrink-1 "
 							: "fas fa-times p-2 flex-shrink-1 hide"
 					}></i>
